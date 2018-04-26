73f8373711f2470617410fc82a1d57f3773de969
Steven Leighton <steven@madetech.com>
2018-04-26 17:26:37 +0100

    Update test to show CI is performing

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe StaticController do
   it 'should display a static landing page' do
     get :homepage
-    expect(response.body).to include('hello world')
+    expect(response.body).to include('hello Hackney!')
   end
 end
