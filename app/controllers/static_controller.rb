dfe4e7c37a5849eb8d9f3bee9eae07ae5bd6e483
Richard Foster <richard@madetech.co.uk>
2018-04-27 10:22:37 +0100

    Change homepage to prove changes have gone through pipeline

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def homepage
-    render status: 200, plain: 'hello Hackney!'
+    render status: 200, plain: 'Deployed with CircleCI'
   end
 end
--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe StaticController do
   it 'should display a static landing page' do
     get :homepage
-    expect(response.body).to include('hello Hackney!')
+    expect(response.body).to include('Deployed with CircleCI')
   end
 end
