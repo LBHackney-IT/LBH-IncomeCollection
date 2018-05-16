af42ad7d1438adfaba3211654ef6018e710cd6db
Steven Leighton <steven@madetech.com>
2018-05-16 16:55:45 +0100

    Add handling for retry limit exceptions

--- lib/hackney/income/anonymizer.rb
+++ lib/hackney/income/anonymizer.rb
@@ -2,41 +2,57 @@ module Hackney
   module Income
     module Anonymizer
       extend self
-
       def anonymize_tenancy(tenancy:)
-        Faker::Config.random = Random.new(tenancy[:tenancy_ref].to_i)
+        begin
+          Faker::Config.random = Random.new(tenancy[:tenancy_ref].to_i)
 
-        tenancy[:primary_contact].merge!(
-          first_name: Faker::Name.unique.first_name,
-          last_name: Faker::Name.unique.last_name,
-          title: Faker::Name.prefix
-        )
+          tenancy[:primary_contact].merge!(
+            first_name: Faker::Name.unique.first_name,
+            last_name: Faker::Name.unique.last_name,
+            title: Faker::Name.prefix
+          )
 
-        tenancy[:address].merge!(
-          address_1: Faker::Address.unique.street_address,
-          post_code: 'H4C KN3Y'
-        )
+          tenancy[:address].merge!(
+            address_1: Faker::Address.unique.street_address,
+            post_code: 'H4C KN3Y'
+          )
 
-        Faker::Config.random = nil
+          Faker::Config.random = nil
+
+          rescue Faker::UniqueGenerator::RetryLimitExceeded
+            reset_unique_generator
+          end
+
         tenancy
       end
 
       def anonymize_tenancy_list_item(tenancy:)
-        Faker::Config.random = Random.new(tenancy[:tenancy_ref].to_i)
+        begin
+          Faker::Config.random = Random.new(tenancy[:tenancy_ref].to_i)
 
-        tenancy[:primary_contact].merge!(
-          first_name: Faker::Name.unique.first_name,
-          last_name: Faker::Name.unique.last_name,
-          title: Faker::Name.prefix
-        )
+          tenancy[:primary_contact].merge!(
+            first_name: Faker::Name.unique.first_name,
+            last_name: Faker::Name.unique.last_name,
+            title: Faker::Name.prefix
+          )
 
-        tenancy[:address_1] = Faker::Address.unique.street_address
-        tenancy[:postcode] = Faker::Address.unique.zip
+          tenancy[:address_1] = Faker::Address.unique.street_address
+          tenancy[:postcode] = Faker::Address.unique.zip
 
-        Faker::Config.random = nil
+          Faker::Config.random = nil
+
+          rescue Faker::UniqueGenerator::RetryLimitExceeded
+            reset_unique_generator
+        end
+
         tenancy
       end
 
+      private
+
+      def reset_unique_generator
+        Faker::UniqueGenerator.clear
+      end
     end
   end
 end
