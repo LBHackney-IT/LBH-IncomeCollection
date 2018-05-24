ecca14c17b8ceed873bbcad805563ee9055f5f0b
Steven Leighton <steven@madetech.com>
2018-05-24 17:03:08 +0100

    Use fake developer tenancy data in development

--- app/controllers/tenancies_controller.rb
+++ app/controllers/tenancies_controller.rb
@@ -6,16 +6,24 @@ class TenanciesController < ApplicationController
 
   def show
     view_tenancy = Hackney::Income::ViewTenancy.new(tenancy_gateway: tenancy_gateway, transactions_gateway: transactions_gateway)
-    @tenancy = view_tenancy.execute(tenancy_ref: params[:id])
+    @tenancy = view_tenancy.execute(tenancy_ref: params.fetch(:id))
   end
 
   private
 
   def tenancy_gateway
-    Hackney::Income::ReallyDangerousTenancyGateway.new(api_host: ENV['INCOME_COLLECTION_API_HOST'])
+    if Rails.env.development?
+      Hackney::Income::TestTenancyGateway.new
+    else
+      Hackney::Income::ReallyDangerousTenancyGateway.new(api_host: ENV['INCOME_COLLECTION_API_HOST'])
+    end
   end
 
   def transactions_gateway
-    Hackney::Income::TransactionsGateway.new(api_host: ENV['INCOME_COLLECTION_API_HOST'])
+    if Rails.env.development?
+      Hackney::Income::StubTransactionsGateway.new
+    else
+      Hackney::Income::TransactionsGateway.new(api_host: ENV['INCOME_COLLECTION_API_HOST'])
+    end
   end
 end
