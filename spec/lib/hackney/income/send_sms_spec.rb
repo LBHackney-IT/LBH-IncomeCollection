c11abe9910077789e6cbc903db5e0233362c4be5
Richard Foster <richard@rpf.me>
2018-05-29 11:30:52 +0100

    Use tenancy gateway in Send SMS use case

--- spec/lib/hackney/income/send_sms_spec.rb
+++ spec/lib/hackney/income/send_sms_spec.rb
@@ -1,67 +1,40 @@
 require 'rails_helper'
 
 describe Hackney::Income::SendSms do
-  let(:tenancy) do
-    Hackney::Tenancy.new.tap do |t|
-      t.ref = '012345/01'
-      t.primary_contact = {
-        first_name: 'Diana',
-        contact_number: '01234 123456'
-      }
-    end
-  end
-
-  let(:template_id) { 'this-is-a-template-id' }
-  let(:notification_gateway) { StubNotificationGateway.new }
-  let(:send_sms) { described_class.new(notification_gateway: notification_gateway) }
+  let(:tenancy_gateway) { Hackney::Income::StubTenancyGateway.new }
+  let(:notification_gateway) { Hackney::Income::StubNotificationsGateway.new }
+  let(:send_sms) { described_class.new(tenancy_gateway: tenancy_gateway, notification_gateway: notification_gateway) }
 
   context 'when sending an SMS manually' do
     subject do
-      send_sms.execute(tenancy: tenancy, template_id: template_id)
+      send_sms.execute(tenancy_ref: '2345678', template_id: 'this-is-a-template-id')
       notification_gateway.last_text_message
     end
 
     it 'should map the tenancy to a set of variables' do
       expect(subject).to include(
         variables: {
-          'first name' => 'Diana'
+          'first name' => 'Bruce'
         }
       )
     end
 
     it 'should pass through the phone number' do
       expect(subject).to include(
-        phone_number: '01234 123456'
+        phone_number: '0208 123 1234'
       )
     end
 
     it 'should pass through the template id' do
       expect(subject).to include(
-        template_id: template_id
+        template_id: 'this-is-a-template-id'
       )
     end
 
     it 'should generate a tenant and message representative reference' do
       expect(subject).to include(
-        reference: 'manual_012345/01'
+        reference: 'manual_2345678'
       )
     end
-  end
-end
-
-class StubNotificationGateway
-  attr_reader :last_text_message
-
-  def initialize
-    @last_text_message = nil
-  end
-
-  def send_text_message(phone_number:, template_id:, reference:, variables:)
-    @last_text_message = {
-      phone_number: phone_number,
-      template_id: template_id,
-      reference: reference,
-      variables: variables
-    }
   end
 end
