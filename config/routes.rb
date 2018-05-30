caabdc57561575e47a5e253710d68baa7aad58a7
Steven Leighton <steven@madetech.com>
2018-05-30 11:21:11 +0100

    Add routes for email actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/tenancies/:id', to: 'tenancies#show', as: :tenancy
   get '/tenancies/:id/sms', to: 'tenancies_sms#show', as: :tenancy_sms
   post '/tenancies/:id/sms', to: 'tenancies_sms#create', as: :create_tenancy_sms
+  get '/tenancies/:id/email', to: 'tenancies_email#show', as: :tenancy_email
+  post '/tenancies/:id/email', to: 'tenancies_email#create', as: :create_tenancy_email
 
   get '/login', to: 'sessions#new', as: :login
   get '/logout', to: 'sessions#destroy', as: :logout
