83624a8438377acd953c3108fe22864519d4990c
Richard Foster <richard@rpf.me>
2018-05-30 10:51:41 +0100

    Install and configure Delayed Job

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_142949) do
+ActiveRecord::Schema.define(version: 2018_05_29_155909) do
 
-  create_table "tenancies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
+  create_table "tenancies", force: :cascade do |t|
     t.string "ref"
     t.index ["ref"], name: "index_tenancies_on_ref", unique: true
   end
 
-  create_table "tenancy_events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tenancy_events", force: :cascade do |t|
     t.string "event_type"
     t.string "description"
     t.boolean "automated"
-    t.bigint "tenancy_id"
+    t.integer "tenancy_id"
     t.index ["tenancy_id"], name: "index_tenancy_events_on_tenancy_id"
   end
 
-  add_foreign_key "tenancy_events", "tenancies"
 end
