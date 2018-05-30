5621ac88e0a7cd321031356d7a2180ad6e397b11
Richard Foster <richard@rpf.me>
2018-05-30 10:53:26 +0100

    Create scheduler gateway for scheduling sms jobs in the future

--- app/controllers/tenancies_controller.rb
+++ app/controllers/tenancies_controller.rb
@@ -14,18 +14,14 @@ class TenanciesController < ApplicationController
   def tenancy_gateway
     Hackney::Income::ReallyDangerousTenancyGateway.new(
       api_host: ENV['INCOME_COLLECTION_API_HOST'],
-      include_developer_data: include_developer_data?
+      include_developer_data: Rails.application.config.include_developer_data?
     )
   end
 
   def transactions_gateway
     Hackney::Income::TransactionsGateway.new(
       api_host: ENV['INCOME_COLLECTION_API_HOST'],
-      include_developer_data: include_developer_data?
+      include_developer_data: Rails.application.config.include_developer_data?
     )
-  end
-
-  def include_developer_data?
-    Rails.env.development? || Rails.env.staging?
   end
 end
