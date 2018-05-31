1e857d50f3b8fa6ea391279181024f8b3ea9ede9
Richard Foster <richard@rpf.me>
2018-05-31 17:40:54 +0100

    Display links in message previews

---
+++ spec/helpers/message_preview_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe MessagePreviewHelper do
+  context '#message_preview' do
+    subject { helper.message_preview(message) }
+
+    context 'when there are newlines in the message' do
+      let(:message) { "dear steven,\nplease get in touch" }
+
+      it 'should convert them into <br> tags' do
+        expect(subject).to eq("<p>dear steven,\n<br />please get in touch</p>")
+      end
+    end
+
+    context 'when there are links in the message' do
+      let(:message) { "please visit https://example.com" }
+
+      it 'should convert them into <a> tags' do
+        expect(subject).to include("<a href=\"https://example.com\">https://example.com</a>")
+      end
+    end
+  end
+end
