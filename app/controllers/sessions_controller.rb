35733745cf4f7bfb35932320fdcaf33b78d7edb4
Steven Leighton <steven@madetech.com>
2018-06-04 11:44:36 +0100

    Add find or create user to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,21 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    session[:current_user] = { name: auth_hash.info.name }
+    @user = find_or_create_user.execute(
+      provider_uid: auth_hash.uid,
+      provider: auth_hash.provider,
+      name: auth_hash.info.name,
+      email: 'FIXME',
+      first_name: 'FIXME',
+      last_name: 'FIXME'
+    )
+
+    session[:current_user] = {
+      id: @user.fetch(:id),
+      name: @user.fetch(:name),
+      email: @user.fetch(:email)
+     }
+
     redirect_to root_path
   end
 
@@ -19,5 +33,13 @@ class SessionsController < ApplicationController
 
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  def find_or_create_user
+    Hackney::Income::FindOrCreateUser.new(users_gateway: users_gateway)
+  end
+
+  def users_gateway
+    Hackney::Income::UsersGateway.new
   end
 end
