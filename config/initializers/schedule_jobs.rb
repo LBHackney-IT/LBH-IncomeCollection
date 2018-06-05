42803ac2b1c5826f586623d2ed31d59b8ecc30fd
Richard Foster <richard@rpf.me>
2018-06-05 15:53:56 +0100

    Only schedule new recurring jobs when starting the web server

--- config/initializers/schedule_jobs.rb
+++ config/initializers/schedule_jobs.rb
@@ -1,6 +1,8 @@
-# Scheduled tasks like SyncTenanciesJob should automatically re-queue their next run, if necessary.
-# This sets up these scheduled tasks for the first time if they haven't already been queued.
+if defined?(Rails::Server)
+  # Scheduled tasks like SyncTenanciesJob should automatically re-queue their next run, if necessary.
+  # This sets up these scheduled tasks for the first time if they haven't already been queued, when booting the application server.
 
-Delayed::Worker.logger = ActiveSupport::Logger.new(STDOUT)
+  Delayed::Worker.logger = ActiveSupport::Logger.new(STDOUT)
 
-SyncTenanciesJob.perform_later if Delayed::Job.where('handler LIKE :condition', condition: '%job_class: SyncTenanciesJob%').none?
+  SyncTenanciesJob.perform_later if Delayed::Job.where('handler LIKE :condition', condition: '%job_class: SyncTenanciesJob%').none?
+end
