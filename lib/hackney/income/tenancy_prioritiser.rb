63c1e132f1c256f1d944a898106dc11f2c45506a
Richard Foster <richard@rpf.me>
2018-06-06 17:16:22 +0100

    WIP

--- lib/hackney/income/tenancy_prioritiser.rb
+++ lib/hackney/income/tenancy_prioritiser.rb
@@ -4,3 +4,7 @@ module Hackney
     end
   end
 end
+
+
+# the business rule is assumed to be >3 broken agreements in the last 3 years
+# the agreements data used here is assumed to be dated back 3 years at maximum
