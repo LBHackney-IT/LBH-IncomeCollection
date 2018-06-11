9a2f55582a0550ee4e30559336bd9e8e27086676
Richard Foster <richard@rpf.me>
2018-06-11 15:03:27 +0100

    Add assigned_tenancies method to real sql tenancy case gateway

--- lib/hackney/income/sql_tenancy_case_gateway.rb
+++ lib/hackney/income/sql_tenancy_case_gateway.rb
@@ -20,6 +20,22 @@ module Hackney
         tenancy = Hackney::Models::Tenancy.find_by(ref: tenancy_ref)
         tenancy.update!(assigned_user_id: user_id)
       end
+
+      def assigned_tenancies(assignee_id:)
+        Hackney::Models::Tenancy
+          .where(assigned_user_id: assignee_id)
+          .map do |tenancy|
+            {
+              tenancy_ref: tenancy.ref,
+              address_1: tenancy.address_1,
+              post_code: tenancy.post_code,
+              current_balance: tenancy.current_balance,
+              primary_contact_first_name: tenancy.primary_contact_first_name,
+              primary_contact_last_name: tenancy.primary_contact_last_name,
+              primary_contact_title: tenancy.primary_contact_title
+            }
+          end
+      end
     end
   end
 end
