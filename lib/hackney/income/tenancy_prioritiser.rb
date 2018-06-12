be4650b682e2b890fff106f2a1d4074f89d91b0a
Steven Leighton <steven@madetech.com>
2018-06-12 16:55:21 +0100

    Initial basic weighting of some individual criteria

--- lib/hackney/income/tenancy_prioritiser.rb
+++ lib/hackney/income/tenancy_prioritiser.rb
@@ -1,12 +1,17 @@
 module Hackney
   module Income
     class TenancyPrioritiser
-      def initialize(tenancy:, transactions:)
+      def initialize(tenancy:, transactions:, weightings:)
         @criteria = Hackney::Income::TenancyPrioritiser::Criteria.new(tenancy, transactions)
+        @weightings = weightings
       end
 
       def assign_priority_band
         Hackney::Income::TenancyPrioritiser::Band.new.execute(criteria: @criteria)
+      end
+
+      def assign_priority_score
+        Hackney::Income::TenancyPrioritiser::Score.new(criteria: @criteria, weightings: @weightings).execute
       end
     end
   end
