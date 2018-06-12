46dfc38373e78fa65f2c9f9533744757f74ebc6b
Steven Leighton <steven@madetech.com>
2018-06-12 12:49:47 +0100

    Refactor band assignment into separate class

---
+++ lib/hackney/income/tenancy_prioritiser/stub_criteria.rb
@@ -0,0 +1,47 @@
+module Hackney
+  module Income
+    class TenancyPrioritiser
+      class StubCriteria
+        attr_accessor :balance, :broken_court_order, :days_in_arrears,
+          :number_of_broken_agreements, :nosp_served, :days_since_last_payment,
+          :payment_date_delta, :payment_amount_delta, :active_agreement
+
+        def balance
+          @balance || 100.00
+        end
+
+        def broken_court_order?
+          @broken_court_order || false
+        end
+
+        def days_in_arrears
+          @days_in_arrears || 7
+        end
+
+        def days_since_last_payment
+          @days_since_last_payment
+        end
+
+        def active_agreement?
+          @active_agreement || false
+        end
+
+        def nosp_served?
+          @nosp_served || false
+        end
+
+        def number_of_broken_agreements
+          @number_of_broken_agreements || 0
+        end
+
+        def payment_amount_delta
+          @payment_amount_delta || 0
+        end
+
+        def payment_date_delta
+          @payment_date_delta || 0
+        end
+      end
+    end
+  end
+end
