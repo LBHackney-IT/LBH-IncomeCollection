22207ea2d783e25bc32423ebd5e4deadf245cf62
Steven Leighton <steven@madetech.com>
2018-06-13 11:05:52 +0100

    Individual contributors to score

--- spec/lib/hackney/income/tenancy_prioritiser/score_spec.rb
+++ spec/lib/hackney/income/tenancy_prioritiser/score_spec.rb
@@ -5,7 +5,7 @@ describe Hackney::Income::TenancyPrioritiser::Score do
   subject { described_class.new(criteria: criteria, weightings: weightings) }
 
   context 'when assigning a score based on all criteria' do
-
+    # FIXME: acceptance test(s) for composite score here or elsewhere?
   end
 
   context 'when examining the breakdown of individual contributions to score' do
@@ -56,6 +56,101 @@ describe Hackney::Income::TenancyPrioritiser::Score do
       criteria.days_since_last_payment = 30
 
       expect(subject.days_since_last_payment).to eq(120)
+    end
+
+    it 'considers difference in amount paid between payments' do
+      weightings.payment_amount_delta = 1
+      criteria.payment_amount_delta = -50
+
+      expect(subject.payment_amount_delta).to eq(-50)
+    end
+
+    it 'applies the delta directly to the score, as a positive delta means paid less than previous payment' do
+      weightings.payment_amount_delta = 1
+      criteria.payment_amount_delta = 150
+
+      expect(subject.payment_amount_delta).to eq(150)
+    end
+
+    it 'considers irregularity in payment date' do
+      weightings.payment_date_delta = 5
+      criteria.payment_date_delta = 3
+
+      expect(subject.payment_date_delta).to eq(15)
+    end
+
+    it 'applies the date delta as if it was positve, as a longer or shorter gap between payments is irregular' do
+      weightings.payment_date_delta = 5
+      criteria.payment_date_delta = -4
+
+      expect(subject.payment_date_delta).to eq(20)
+    end
+
+    it 'applies a score addition to a broken agreement' do
+      weightings.number_of_broken_agreements = 50
+      criteria.number_of_broken_agreements = 1
+
+      expect(subject.number_of_broken_agreements).to eq(50)
+    end
+
+    it 'applies greater penalties as the number of agreements gets higher' do
+      weightings.number_of_broken_agreements = 50
+      criteria.number_of_broken_agreements = 5
+
+      expect(subject.number_of_broken_agreements).to eq(300)
+    end
+
+    it 'applies a score to having a live agreement' do
+      weightings.active_agreement = -100
+      criteria.active_agreement = true
+
+      expect(subject.active_agreement).to eq(-100)
+    end
+
+    it 'will apply the live agreement weighting directly' do
+      weightings.active_agreement = 100
+      criteria.active_agreement = true
+
+      expect(subject.active_agreement).to eq(100)
+    end
+
+    it 'will apply a score to having a broken court ordered agreement' do
+      weightings.broken_court_order = 200
+      criteria.broken_court_order = true
+
+      expect(subject.broken_court_order).to eq(200)
+    end
+
+    it 'will not apply a score if broken agreements are not court-ordered' do
+      weightings.broken_court_order = 300
+      criteria.broken_court_order = false
+      criteria.number_of_broken_agreements = 2
+
+      expect(subject.broken_court_order).to eq(nil)
+    end
+
+    it 'will apply a score if there is a valid nosp' do
+      weightings.nosp_served = 20
+      criteria.nosp_served = true
+
+      expect(subject.nosp_served).to eq(20)
+    end
+
+    it 'will apply a score if there is an active nosp' do
+      weightings.active_nosp = 50
+      criteria.active_nosp = true
+
+      expect(subject.active_nosp).to eq(50)
+    end
+
+    it 'will apply active nosp score over valid nosp' do
+      weightings.active_nosp = 100
+      criteria.active_nosp = true
+      weightings.nosp_served = 250
+      criteria.nosp_served = true
+
+      expect(subject.active_nosp).to eq(100)
+      expect(subject.nosp_served).to eq(nil)
     end
   end
 end
