f6151033e9d7b4b01b3516e56d5756ebc0bf5bb4
Steven Leighton <steven@madetech.com>
2018-09-04 14:12:19 +0100

    Update gateway

--- lib/hackney/income/action_diary_entry_gateway.rb
+++ lib/hackney/income/action_diary_entry_gateway.rb
@@ -1,7 +1,44 @@
+require 'uri'
+require 'net/http'
+
 module Hackney
   module Income
     class ActionDiaryEntryGateway
+      def initialize(api_host:, api_key:)
+        @api_host = api_host
+        @api_key = api_key
+      end
+
       def create_action_diary_entry(tenancy_ref:, balance:, code:, type:, date:, comment:, universal_housing_username:)
+        body_data = {
+          arrearsAction: {
+            actionBalance: balance,
+            actionCategory: type,
+            actionCode: code,
+            comment: comment,
+            confirmation: true,
+            deferredTo: Date.today,
+            isCommentOnly: true,
+            newSetRequired: true,
+            tenancyAgreementRef: tenancy_ref
+          },
+          companyCode: 'FIXME',
+          directUser: {
+            userName: 'FIXME',
+            userPassword: 'FIXME'
+          },
+          masterPassword: 'FIXME',
+          sessionToken: 'FIXME',
+          sourceSystem: 'FIXME',
+          userId: 0
+        }.to_json
+
+        uri = URI.parse("#{@api_host}/tenancies/arrears-action-diary")
+        req = Net::HTTP::Post.new(uri.path)
+        req['Content-Type'] = 'application/json'
+        req['x-api-key'] = @api_key
+
+        res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) { |http| http.request(req, body_data) }
       end
     end
   end
--- spec/lib/hackney/income/action_diary_entry_gateway_spec.rb
+++ spec/lib/hackney/income/action_diary_entry_gateway_spec.rb
@@ -1,7 +1,50 @@
 require 'rails_helper'
 
 describe Hackney::Income::ActionDiaryEntryGateway do
-  it 'should have a test' do
-    expect(true).to eq(true)
+  subject { described_class.new(api_host: 'https://example.com/api', api_key: 'skeleton') }
+  let(:request_content) do
+    {
+      arrearsAction: {
+        actionBalance: Faker::Number.decimal(2),
+        actionCategory: Faker::Lorem.characters(3),
+        actionCode: Faker::Lorem.characters(3),
+        comment: Faker::Lorem.words(10),
+        confirmation: true,
+        deferredTo: Date.today,
+        isCommentOnly: true,
+        newSetRequired: true,
+        tenancyAgreementRef: Faker::Lorem.characters(6)
+      },
+      companyCode: 'FIXME',
+      directUser: {
+        userName: 'FIXME',
+        userPassword: 'FIXME'
+      },
+      masterPassword: 'FIXME',
+      sessionToken: 'FIXME',
+      sourceSystem: 'FIXME',
+      userId: 0
+    }
+  end
+
+  before do
+    stub_request(:post, "https://example.com/api/tenancies/arrears-action-diary")
+  end
+
+  context 'sending a request to the API' do
+    it 'should send the required JSON body' do
+      subject.create_action_diary_entry(
+        tenancy_ref: request_content.dig(:arrearsAction).fetch(:tenancyAgreementRef),
+        balance: request_content.dig(:arrearsAction).fetch(:actionBalance),
+        code: request_content.dig(:arrearsAction).fetch(:actionCode),
+        type: request_content.dig(:arrearsAction).fetch(:actionCategory),
+        date: request_content.dig(:arrearsAction).fetch(:deferredTo),
+        comment: request_content.dig(:arrearsAction).fetch(:comment),
+        universal_housing_username: request_content.dig(:directUser).fetch(:userName)
+      )
+
+      assert_requested(:post, 'https://example.com/api/tenancies/arrears-action-diary',
+        :headers => { 'Content-Type' => 'application/json', 'x-api-key' => 'skeleton' }, :body => request_content.to_json, :times => 1)
+    end
   end
 end
