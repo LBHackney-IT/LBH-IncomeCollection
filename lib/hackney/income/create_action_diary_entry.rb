e479631634d88ada184938c8f8fe21df0c78029c
Steven Leighton <steven@madetech.com>
2018-09-04 11:00:57 +0100

    Add use case

---
+++ lib/hackney/income/create_action_diary_entry.rb
@@ -0,0 +1,21 @@
+module Hackney
+  module Income
+    class CreateActionDiaryEntry
+      def initialize(action_diary_gateway:)
+        @action_diary_gateway = action_diary_gateway
+      end
+
+      def execute(tenancy_ref:, balance:, code:, type:, date:, comment:, universal_housing_username:)
+          @action_diary_gateway.create_action_diary_entry(
+          tenancy_ref: tenancy_ref,
+          balance: balance,
+          code: code,
+          type: type,
+          date: date,
+          comment: comment,
+          universal_housing_username: universal_housing_username
+        )
+      end
+    end
+  end
+end
---
+++ spec/lib/hackney/income/create_action_diary_entry_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe Hackney::Income::CreateActionDiaryEntry do
+  let(:action_diary_gateway) { Hackney::Income::StubActionDiaryEntryGateway.new }
+  let(:params) do
+    {
+      tenancy_ref: Faker::Lorem.characters(6),
+      balance: Faker::Number.decimal(2).to_s,
+      code: Faker::Lorem.characters(2),
+      type: Faker::Seinfeld.character,
+      date: Date.today.strftime("%YYYY-%MM-%DD"),
+      comment: Faker::Seinfeld.quote,
+      universal_housing_username: Faker::Seinfeld.character,
+    }
+  end
+
+  subject { described_class.new(action_diary_gateway: action_diary_gateway) }
+
+  it 'should pass the required data through to the gateway' do
+     expect(action_diary_gateway).to receive(:create_action_diary_entry).with(
+       tenancy_ref: params.fetch(:tenancy_ref),
+       balance: params.fetch(:balance),
+       code: params.fetch(:code),
+       type: params.fetch(:type),
+       date: params.fetch(:date),
+       comment: params.fetch(:comment),
+       universal_housing_username: params.fetch(:universal_housing_username)
+     )
+
+     subject.execute(
+       tenancy_ref: params.fetch(:tenancy_ref),
+       balance: params.fetch(:balance),
+       code: params.fetch(:code),
+       type: params.fetch(:type),
+       date: params.fetch(:date),
+       comment: params.fetch(:comment),
+       universal_housing_username: params.fetch(:universal_housing_username)
+     )
+  end
+end
