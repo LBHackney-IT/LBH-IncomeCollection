2f0984fa21e9fd0a60fc046d5aea6bd4a8579e3c
Steven Leighton <steven@madetech.com>
2018-09-04 14:12:02 +0100

    Update controller and view

--- app/controllers/action_diary_entry_controller.rb
+++ app/controllers/action_diary_entry_controller.rb
@@ -1,10 +1,12 @@
 class ActionDiaryEntryController < ApplicationController
   def show
     @tenancy = tenancy_gateway.get_tenancy(tenancy_ref: params.fetch(:id))
+    @type_options = type_options
+    @code_options = code_options
   end
 
   def create
-    use_case = Hackney::Income::CreateActionDiaryEntry.new
+    use_case = Hackney::Income::CreateActionDiaryEntry.new(action_diary_gateway: action_diary_gateway)
     use_case.execute(
       tenancy_ref: params.fetch(:tenancy_ref),
       balance: params.fetch(:balance),
@@ -21,10 +23,31 @@ class ActionDiaryEntryController < ApplicationController
 
   private
 
+  def action_diary_gateway
+    Hackney::Income::ActionDiaryEntryGateway.new(
+      api_host: ENV['INCOME_COLLECTION_API_HOST'],
+      api_key: ENV['INCOME_COLLECTION_API_KEY'],
+    )
+  end
+
   def tenancy_gateway
     Hackney::Income::LessDangerousTenancyGateway.new(
       api_host: ENV['INCOME_COLLECTION_API_HOST'],
       api_key: ENV['INCOME_COLLECTION_API_KEY'],
     )
+  end
+
+  def type_options
+    [
+      ['General Note', 'GEN'],
+      ['SYSTEM', 'SYS']
+    ]
+  end
+
+  def code_options
+    [
+      ['GEN', 'GEN'],
+      ['Z00', 'Z00']
+    ]
   end
 end
