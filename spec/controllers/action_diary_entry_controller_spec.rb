5e0d4e05cc93c820d44a7d498bbfe736913c51c0
Steven Leighton <steven@madetech.com>
2018-09-04 11:01:19 +0100

    Tidy up

--- spec/controllers/action_diary_entry_controller_spec.rb
+++ spec/controllers/action_diary_entry_controller_spec.rb
@@ -5,36 +5,16 @@ describe ActionDiaryEntryController do
     stub_authentication
     stub_const('Hackney::Income::LessDangerousTenancyGateway', Hackney::Income::StubTenancyGatewayBuilder.build_stub)
     stub_const('Hackney::Income::CreateActionDiaryEntry', Hackney::Income::StubCreateActionDiaryEntry)
-    # stub_const('Hackney::Income::GovNotifyGateway', Hackney::Income::StubNotificationsGateway)
-    # stub_const('Hackney::Income::TransactionsGateway', Hackney::Income::StubTransactionsGateway)
-    # stub_const('Hackney::Income::SqlEventsGateway', Hackney::Income::StubEventsGateway)
-    # stub_const('Hackney::Income::SchedulerGateway', Hackney::Income::StubSchedulerGateway)
   end
-  #
-  # context '#show' do
-  #   it 'should render a form for creating action diary entries' do
-  #     get :show, params: { id: '3456789' }
-  #
-  #     expect(subject).to render_template(:show)
-  #   end
-  #
-  #   # it 'displays basic tenant details' do
-  #   #   get :show, params: { id: '3456789' }
-  #   #
-  #   #   expect(assigns(:tenancy)).to be_present
-  #   #   expect(assigns(:tenancy)).to be_instance_of(Hackney::Income::Domain::Tenancy)
-  #   #   expect(assigns(:tenancy)).to be_valid
-  #   # end
-  # end
 
   context 'creating an action diary entry successfully' do
     it 'should call the send sms use case correctly' do
       expect_any_instance_of(Hackney::Income::CreateActionDiaryEntry).to receive(:execute).with(
         tenancy_ref: '3456789',
-        balance: 100.12,
+        balance: 100.12.to_s,
         code: 'GEN',
         type: 'General Note',
-        date: Date.today,
+        date: Date.today.strftime("%YYYY-%MM-%DD"),
         comment: 'Test comment',
         universal_housing_username: 'RFoster',
       )
@@ -45,7 +25,6 @@ describe ActionDiaryEntryController do
         balance: 100.12,
         code: 'GEN',
         type: 'General Note',
-        date: Date.today,
         comment: 'Test comment',
         universal_housing_username: 'RFoster'
       }
