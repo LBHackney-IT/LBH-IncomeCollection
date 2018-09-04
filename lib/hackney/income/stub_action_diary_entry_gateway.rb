c83dc9c3807e21a950c666075f40b1e837f09e89
Steven Leighton <steven@madetech.com>
2018-09-04 11:00:21 +0100

    Add gateway

---
+++ lib/hackney/income/stub_action_diary_entry_gateway.rb
@@ -0,0 +1,8 @@
+module Hackney
+  module Income
+    class StubActionDiaryEntryGateway
+      def create_action_diary_entry(tenancy_ref:, balance:, code:, type:, date:, comment:, universal_housing_username:)
+      end
+    end
+  end
+end
---
+++ spec/lib/hackney/income/action_diary_entry_gateway_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Hackney::Income::ActionDiaryEntryGateway do
+  it 'should have a test' do
+    expect(true).to eq(true)
+  end
+end
