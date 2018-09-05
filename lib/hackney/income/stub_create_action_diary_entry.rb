d5707110945ddfc4b064cbfc2eae09325b0aff4d
Richard Foster <richard@rpf.me>
2018-09-05 15:14:46 +0100

    Clean up

--- lib/hackney/income/stub_create_action_diary_entry.rb
+++ lib/hackney/income/stub_create_action_diary_entry.rb
@@ -5,19 +5,7 @@ module Hackney
       end
 
       def execute(tenancy_ref:, balance:, code:, type:, date:, comment:, universal_housing_username:)
-        # @last_call = {
-        #   tenancy_ref: tenancy_ref,
-        #   balance: balance,
-        #   code: code,
-        #   type: type,
-        #   comment: comment,
-        #   universal_housing_username: universal_housing_username
-        # }
       end
-
-      # def last_call
-      #   @last_call
-      # end
     end
   end
 end
