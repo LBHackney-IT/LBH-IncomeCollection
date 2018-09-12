a80697d09251c8c8255b9c0120b4372371169706
Richard Foster <richard@rpf.me>
2018-09-12 18:58:41 +0100

    Add feature spec around visiting the homepage

--- spec/features/creating_action_diary_entry_spec.rb
+++ spec/features/creating_action_diary_entry_spec.rb
@@ -1,22 +1,11 @@
 require 'rails_helper'
 
 describe 'creating action diary entry' do
-  let(:provider_uid) { Faker::Number.number(12).to_s }
-  let(:extra_hash) { { 'raw_info' => { 'id_token' => "#{Faker::Number.number(6)}.123456ABC" } } }
-  let(:info_hash) do
-    {
-      'name' => Faker::StarTrek.character,
-      'email' => "#{Faker::StarTrek.character}@enterprise.fed.gov",
-      'first_name' => Faker::StarTrek.specie,
-      'last_name' => Faker::StarTrek.villain
-    }
-  end
+  let(:username) { Faker::StarTrek.character }
 
   before { stub_use_cases }
   around do |example|
-    stub_authentication do
-      example.run
-    end
+    with_mock_authentication(username: username) { example.run }
   end
 
   context 'filling in the form as a user' do
@@ -28,7 +17,7 @@ describe 'creating action diary entry' do
         type: '',
         date: Date.today.strftime('%YYYY-%MM-%DD'),
         comment: 'Test comment.',
-        universal_housing_username: info_hash.fetch('name')
+        universal_housing_username: username
       )
 
       visit '/auth/azureactivedirectory'
@@ -42,27 +31,6 @@ describe 'creating action diary entry' do
       fill_in 'comment', with: 'Test comment.'
       click_button 'Add action'
     end
-  end
-
-  def stub_authentication(&block)
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.add_mock(:azureactivedirectory)
-    OmniAuth.config.mock_auth[:azureactivedirectory] = OmniAuth::AuthHash.new(
-      'provider' => 'azureactivedirectory',
-      'uid' => provider_uid,
-      'info' => info_hash,
-      'extra' => extra_hash
-    )
-
-    ENV['IC_STAFF_GROUP'] = '123456ABC'
-
-    yield block
-
-    OmniAuth.config.test_mode = false
-    OmniAuth.config.mock_auth.delete(:azureactivedirectory)
-    Rails.application.env_config.delete('omniauth.auth')
-
-    ENV['IC_STAFF_GROUP'] = nil
   end
 
   def stub_use_cases
