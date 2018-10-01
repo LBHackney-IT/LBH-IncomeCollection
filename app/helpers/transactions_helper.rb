c9d47ecc5ed06cab4f6c0c999314215fc1401738
Steven Leighton <steven@madetech.com>
2018-10-01 18:04:46 +0200

    User a helper for display amount

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -12,4 +12,8 @@ module TransactionsHelper
       transaction.fetch(:date) >= last_year
     end
   end
+
+  def display_amount(charge)
+    charge.positive? ? format('£%.2f', charge) : format('-£%.2f', charge.abs)
+  end
 end
