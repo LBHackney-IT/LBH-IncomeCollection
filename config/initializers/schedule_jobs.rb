d6cb1abea700c5a4a600d8927e0b9ab478c3c087
cob16 <public+github@cormacbrady.info>
2018-10-08 12:24:22 +0100

    delete all mention of tenancies sync as this is moved to api

--- config/initializers/schedule_jobs.rb
+++ config/initializers/schedule_jobs.rb
@@ -1,8 +1,5 @@
 if defined?(Rails::Server)
-  # Scheduled tasks like SyncTenanciesJob should automatically re-queue their next run, if necessary.
   # This sets up these scheduled tasks for the first time if they haven't already been queued, when booting the application server.
 
   Delayed::Worker.logger = ActiveSupport::Logger.new(STDOUT)
-
-  SyncTenanciesJob.perform_later if Delayed::Job.where('handler LIKE :condition', condition: '%job_class: SyncTenanciesJob%').none?
 end
