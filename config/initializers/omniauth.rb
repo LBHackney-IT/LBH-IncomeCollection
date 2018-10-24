f41e3a2fdfd4b1b27cb879d21d2afd408ef85e63
Steven Leighton <steven@madetech.com>
2018-10-24 15:29:17 +0100

    Use a new style proc in initialiser

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
-
+OmniAuth.config.on_failure = proc { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :azure_activedirectory, ENV['AAD_CLIENT_ID'], ENV['AAD_TENANT']
 end
