e572ac5526eedf8266f2110bff5a1459b1e0fde4
Elena <elena@madetech.com>
2018-11-07 11:59:09 +0000

    sending sms via api

--- lib/hackney/income/gov_notify_gateway.rb
+++ lib/hackney/income/gov_notify_gateway.rb
@@ -3,19 +3,40 @@ require 'notifications/client'
 module Hackney
   module Income
     class GovNotifyGateway
-      def initialize(sms_sender_id:, api_key:)
+      # def initialize(sms_sender_id:, api_key:)
+      #   @sms_sender_id = sms_sender_id
+      #   @client = Notifications::Client.new(api_key)
+      # end
+      def initialize(api_host:, api_key:, sms_sender_id:)
+        @api_host = api_host
+        @api_key = api_key
         @sms_sender_id = sms_sender_id
-        @client = Notifications::Client.new(api_key)
+
+        # sms_sender_id: ENV['GOV_NOTIFY_SENDER_ID'],
+        # @sms_sender_id = sms_sender_id
+        @client = Notifications::Client.new(ENV['GOV_NOTIFY_API_KEY'])
       end
 
       def send_text_message(phone_number:, template_id:, reference:, variables:)
-        @client.send_sms(
+        uri = URI("#{@api_host}/messages/send_sms")
+        uri.query = URI.encode_www_form(
           phone_number: pre_release_phone_number(phone_number),
           template_id: template_id,
           personalisation: variables,
           reference: reference,
           sms_sender_id: @sms_sender_id
         )
+
+        req = Net::HTTP::Post.new(uri)
+        req['X-Api-Key'] = @api_key
+
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
+
+        unless res.is_a? Net::HTTPSuccess
+          raise Exceptions::IncomeApiError.new(res), "error sending sms"
+        end
+
+        res
       end
 
       def send_email(recipient:, template_id:, reference:, variables:)
