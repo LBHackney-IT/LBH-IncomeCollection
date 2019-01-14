570caa9351f8f7ab3f13c2d8f5d191436e8886e0
cob16 <cormac.brady@hotmail.co.uk>
2019-01-14 13:31:42 +0000

    remove redundant helper method

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -12,8 +12,4 @@ module TransactionsHelper
       transaction.fetch(:date) >= last_year
     end
   end
-
-  def display_amount(charge)
-    charge.positive? ? format('£%.2f', charge) : format('-£%.2f', charge.abs)
-  end
 end
