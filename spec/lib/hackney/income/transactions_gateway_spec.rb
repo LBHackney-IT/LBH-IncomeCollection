37b8224ffba2e71e1ac2e014684a08ff47412e31
Elena <elena@madetech.com>
2019-04-03 15:23:16 +0100

    tidy method correctly parsing thousands

--- spec/lib/hackney/income/transactions_gateway_spec.rb
+++ spec/lib/hackney/income/transactions_gateway_spec.rb
@@ -29,9 +29,18 @@ describe Hackney::Income::TransactionsGateway do
             property_ref: '000123',
             type: 'RNT',
             date: '2018-03-26T00:00:00',
-            amount: '¤133.75',
+            amount: '¤1,133.75',
             transactionID: '0d4911d2-ce30-e811-1234-70106faa6a11',
             description: 'Total Rent'
+          }, {
+            ref: '00123456',
+            transactionSid: nil,
+            property_ref: '001234',
+            type: 'RCP',
+            date: '2018-03-26T00:00:00',
+            amount: '(¤1,100.75)',
+            transactionID: '0d4911d2-ce30-e811-1234-70106faa6a11',
+            description: 'Debit / Credit Card'
           }]
         }.to_json)
     end
@@ -42,13 +51,21 @@ describe Hackney::Income::TransactionsGateway do
     end
 
     it 'should include a transaction' do
-      expect(subject).to include(
-        id: '000123',
-        timestamp: Date.new(2018, 3, 26),
-        tenancy_ref: '000123/01',
-        description: 'Total Rent',
-        value: 133.75,
-        type: 'RNT'
+      expect(subject).to eq(
+        [{
+          id: '000123',
+          timestamp: Date.new(2018, 3, 26),
+          tenancy_ref: '000123/01',
+          description: 'Total Rent',
+          value: 1133.75,
+          type: 'RNT'
+ },
+         { id: '001234',
+           timestamp: Date.new(2018, 3, 26),
+           tenancy_ref: '000123/01',
+           description: 'Debit / Credit Card',
+           value: -1100.75,
+           type: 'RCP' }]
       )
     end
   end
