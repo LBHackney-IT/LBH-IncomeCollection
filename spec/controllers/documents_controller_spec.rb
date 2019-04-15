58d50722468d1cfe5d4221b7829f94ca80832bf5
Elena <elena@madetech.com>
2019-04-15 14:01:07 +0100

    testing

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -12,23 +12,44 @@ describe DocumentsController do
   let(:template_name) { Faker::StarTrek.character }
   let(:payment_ref) { Faker::IDNumber.valid }
 
-  context '#show' do
-    it 'downloads a document' do
-      expect_any_instance_of(Hackney::Income::DocumentsGateway).to receive(:download_document).with(
-        id: id
-      ).and_return(Net::HTTPOK.new(1.1, 200, 'nil'))
+  let(:document_response) { Net::HTTPResponse.new(1.1, 200, 'OK') }
+  let(:res_body) { Faker::StarTrek.villain }
+  let(:res_content_type) { 'application/pdf' }
+  let(:res_content_disposition) { "attachment; filename=\"#{Faker::StarTrek.location}.pdf\"" }
 
-      get :show, params: { id: id }
+  context '#show' do
+    context 'when downloading document' do
+      before do
+        document_response['Content-Disposition'] = res_content_disposition
+        allow(document_response).to receive(:body).and_return(res_body)
+        allow(document_response).to receive(:content_type).and_return(res_content_type)
+
+        expect_any_instance_of(Hackney::Income::DocumentsGateway).to receive(:download_document).with(
+          id: id
+        ).and_return(document_response)
+
+        get :show, params: { id: id }
+      end
+
+      it { expect(response.content_type).to eq(res_content_type) }
+
+      it { expect(response.header['Content-Disposition']).to eq(res_content_disposition) }
+
+      it { expect(response.body).to eq(res_body) }
     end
 
-    it 'shows an error message when a document is not found' do
-      expect_any_instance_of(Hackney::Income::DocumentsGateway).to receive(:download_document).with(
-        id: id
-      ).and_return(Net::HTTPOK.new(1.1, 404, nil))
+    context 'when not found' do
+      before do
+        expect_any_instance_of(Hackney::Income::DocumentsGateway).to receive(:download_document).with(
+          id: id
+        ).and_return(Net::HTTPOK.new(1.1, 404, nil))
 
-      get :show, params: { id: id }
+        get :show, params: { id: id }
+      end
 
-      expect(flash[:notice]).to eq('Document is not found')
+      it { expect(response).to redirect_to documents_path }
+
+      it { expect(flash[:notice]).to eq('Document not found') }
     end
   end
 
