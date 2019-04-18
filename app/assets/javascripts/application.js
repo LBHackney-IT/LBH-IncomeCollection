5bc7ddb205c6efd1ebbf152fe9b6bcb4c018413a
Elena <elena@madetech.com>
2019-04-18 10:54:47 +0100

    wip js fun

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,88 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+function send_all(list_of_refs, template_id) {
+
+  Rails.ajax({
+    url: "/letters/preview.js",
+    type: "POST",
+    data: "first_name=dfhdfjhdfjhjhfdjhfdy",
+    success: function(data) {
+      console.log(data);
+      console.log( "fart!" );
+
+    }
+  });
+
+
+  // fetch('/letters/preview.js', {
+  //   method: 'post',
+  //   body: JSON.stringify({first_name: "Ricky", last_name: "Bobby"}),
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'X-CSRF-Token': Rails.csrfToken()
+  //   },
+  //   credentials: 'same-origin'
+  // }).then(function(response) {
+  //   return response.json();
+  // }).then(function(data) {
+  //   console.log(data);
+  // });
+
+
+
+
+  // $.ajax({
+  //   url: '/letters/preview.js',
+  //   type: 'post',
+  //   dataType: 'json',
+  //   contentType: 'application/json',
+  //   success: function (data) {
+  //     $('#target').html(data.msg);
+  //     console.log('wowowo') ;
+  //
+  //   },
+  //   complete: function (data) {
+  //     console.log('wowowowww222ooo') ;
+  //
+  //   },
+  //   data: JSON.stringify({
+  //     payment_ref: 'js',
+  //     authenticity_token:"p2J+yENid/4LbF4WhdSeA/Y/70KgIclXIAn051dG8zAlSj2Qg3ePKJGTh+h71+liQj2QAIpTCa1fn7+iQyZl8Q==",
+  //     template_id: 'letter_1_in_arrears_FH_agreed_template'
+  //   })
+  // }).done(function() {
+  //   console.log('wooo') ;
+  //   $( this ).append( "done" );
+  // });
+}
+
+// $.ajax({
+//   url: "https://fiddle.jshell.net/favicon.png",
+//   beforeSend: function( xhr ) {
+//     xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+//   }
+// })
+//   .done(function( data ) {
+//     if ( console && console.log ) {
+//       console.log( "Sample of data:", data.slice( 0, 100 ) );
+//     }
+//   });
+
+function s_table(one, two)
+{
+  $("#successful_table").append("<tr><td>"+one+"</td><td>"+two+"</td></tr>");
+}
+// letters = {
+//   init: function(listOfRefs) {
+//     this.paymentRef = listOfRefs
+//   },
+//   // name: 'Chris',
+//   // age: 38,
+//   // greeting: function() {
+//   //   alert('Hi! I\'m ' + this.name + '.');
+//   // }
+// };
--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,9 +1,12 @@
 class LettersController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def new
     @letter_templates = use_cases.list_letter_templates.execute
   end
 
   def preview
+
     @payment_refs = payment_refs
 
     @preview = use_cases.get_letter_preview.execute(
@@ -11,8 +14,14 @@ class LettersController < ApplicationController
       pay_ref: @payment_refs.delete_at(0),
       user_id: session[:current_user].fetch('id')
     )
-    flash[:notice] = 'Payment reference not found' if @preview[:status_code] == 404
-    redirect_to letters_new_path if @preview[:status_code] == 404
+byebug
+    respond_to do |format|
+      format.html do
+        flash[:notice] = 'Payment reference not found' if @preview[:status_code] == 404
+        redirect_to letters_new_path if @preview[:status_code] == 404
+      end
+      format.js {}
+    end
   end
 
   def send_letter
@@ -29,6 +38,6 @@ class LettersController < ApplicationController
   private
 
   def payment_refs
-    params.require(:pay_refs).split(',').collect(&:strip)
+    params.require(:pay_refs).split(',').collect(&:to_i)
   end
 end
