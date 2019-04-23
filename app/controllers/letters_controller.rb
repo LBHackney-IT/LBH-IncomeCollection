a0daa36db6f6f04a8c013502c261dfafce4cdb8a
Elena <elena@madetech.com>
2019-04-23 17:03:23 +0100

    processing all payref and sending them one by one

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -24,28 +24,28 @@ class LettersController < ApplicationController
       pay_ref: params.require(:pay_ref),
       user_id: session[:current_user].fetch('id')
     )
+
+    head(@preview[:status_code]) if @preview[:status_code]
+
     respond_to do |format|
       format.js
     end
   end
 
   def send_letter
-    @letter_uuid = params.require(:uuid)
-    # response = use_cases.send_letter.execute(
-    #   uuid: @letter_uuid,
-    #   user_id: session[:current_user].fetch('id')
-    # )
-    respond_to do |format|
-      # if response.code.to_i == 204
-        format.html { redirect_to letters_new_path, notice: 'Successfully sent' }
-        format.js   {}
-      # end
-    end
+    response = use_cases.send_letter.execute(
+      uuid: params.require(:uuid),
+      user_id: session[:current_user].fetch('id')
+    )
+
+    flash[:notice] = 'Successfully sent' if response.code.to_i == 204
+
+    redirect_to letters_new_path
   end
 
   private
 
   def payment_refs
-    params.require(:pay_refs).split(',').collect(&:to_i)
+    params.require(:pay_refs).split(',').map(&:strip)
   end
 end
