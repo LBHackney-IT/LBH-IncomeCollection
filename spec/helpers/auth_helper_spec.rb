5181aedb78c5cf62778ceccd4c7bd98308cf38d9
Richard Race <richard.race@madetech.com>
2019-09-11 10:18:44 +0100

    Whitespace

--- spec/helpers/auth_helper_spec.rb
+++ spec/helpers/auth_helper_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 describe AuthHelper do
   describe '#auth_provider_path' do
-    context 'when in development environment' do 
+    context 'when in development environment' do
       before do
         allow(Rails.env).to receive(:development?).and_return(true)
-      end 
+      end
 
       context 'when MAGIC VAR is set' do
         it 'returns the developer provider path' do
           allow(ENV).to receive(:[]).with('AUTH_NO_AZURE_AD').and_return('true')
-          expect(helper.auth_provider_path).to eq('/auth/developer')        
+          expect(helper.auth_provider_path).to eq('/auth/developer')
         end
       end
 
       context 'when MAGIC VAR is not set' do
         it 'returns the azure ad provider path' do
           allow(ENV).to receive(:[]).with('AUTH_NO_AZURE_AD').and_return(nil)
-          expect(helper.auth_provider_path).to eq('/auth/azureactivedirectory')        
+          expect(helper.auth_provider_path).to eq('/auth/azureactivedirectory')
         end
       end
     end
