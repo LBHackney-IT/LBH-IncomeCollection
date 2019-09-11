ba10f8d22ac6e54b5f936e1fee3aba473beb0dc1
Elena <elena@madetech.com>
2019-09-11 14:58:18 +0100

    test dry up

--- spec/helpers/auth_helper_spec.rb
+++ spec/helpers/auth_helper_spec.rb
@@ -2,29 +2,38 @@ require 'rails_helper'
 
 describe AuthHelper do
   describe '#auth_provider_path' do
+    before do
+      allow(Rails.env).to receive(:development?).and_return(rails_env)
+    end
+
     context 'when in development environment' do
+      let(:rails_env) { true }
+
       before do
-        allow(Rails.env).to receive(:development?).and_return(true)
+        allow(ENV).to receive(:fetch).with('AUTH_NO_AZURE_AD', false).and_return(auth_no_azure_ad)
       end
 
-      context 'when MAGIC VAR is set' do
+      context 'when AUTH_NO_AZURE_AD is set' do
+        let(:auth_no_azure_ad) { true }
+
         it 'returns the developer provider path' do
-          allow(ENV).to receive(:fetch).with('AUTH_NO_AZURE_AD', false).and_return(true)
           expect(helper.auth_provider_path).to eq('/auth/developer')
         end
       end
 
-      context 'when MAGIC VAR is not set' do
+      context 'when AUTH_NO_AZURE_AD is not set' do
+        let(:auth_no_azure_ad) { false }
+
         it 'returns the azure ad provider path' do
-          allow(ENV).to receive(:fetch).with('AUTH_NO_AZURE_AD', false).and_return(false)
           expect(helper.auth_provider_path).to eq('/auth/azureactivedirectory')
         end
       end
     end
 
     context 'when not in development environment' do
+      let(:rails_env) { false }
+
       it 'returns the azure ad provider path' do
-        allow(Rails.env).to receive(:development?).and_return(false)
         expect(helper.auth_provider_path).to eq('/auth/azureactivedirectory')
       end
     end
