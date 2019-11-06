0f5c660594a5b5bcde267d5e68cda342a44c9ebf
Richard Race <richard.race@madetech.com>
2019-11-06 17:16:11 +0000

    Do not display tenancies for Leasehold users.

---
+++ spec/request/worktray_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+describe 'Viewing the Worktray', type: :request do
+  before do
+    jwt_token = build_jwt_token(groups: groups)
+    cookies['hackneyToken'] = jwt_token
+
+    stub_my_cases_response
+  end
+
+  context 'with a leasehold services user' do
+    let(:groups) { ['leasehold-services-group-1'] }
+
+    it "doesn't render the worktray tabs" do
+      get worktray_path
+
+      expect(response).not_to render_template('tenancies/worktray/tabs')
+    end
+
+    it "doesn't render the worktray table" do
+      get worktray_path
+
+      expect(response).not_to render_template('tenancies/worktray/worktray_table')
+    end
+
+    it 'does render the leasehold button' do
+      get worktray_path
+
+      expect(response).not_to render_template('leasehold/buttons')
+    end
+  end
+
+  context 'with a income collection user' do
+    let(:groups) { ['income-collection-group-1'] }
+
+    it 'does render the worktray tabs' do
+      get worktray_path
+
+      expect(response).not_to render_template('tenancies/worktray/tabs')
+    end
+
+    it 'does render the worktray table' do
+      get worktray_path
+
+      expect(response).not_to render_template('tenancies/worktray/worktray_table')
+    end
+
+    it 'does not render the leasehold button' do
+      get worktray_path
+
+      expect(response).not_to render_template('leasehold/buttons')
+    end
+  end
+
+  private
+
+  def stub_my_cases_response(override_params = {})
+    stub_const('Hackney::Income::IncomeApiUsersGateway', Hackney::Income::StubIncomeApiUsersGateway)
+
+    response_json = File.read(Rails.root.join('spec', 'examples', 'my_cases_response.json'))
+
+    default_filters = {
+      is_paused: false,
+      number_per_page: 20,
+      page_number: 1,
+      full_patch: false,
+      patch: nil,
+      recommended_actions: nil,
+      upcoming_court_dates: false,
+      upcoming_evictions: false
+    }.merge(override_params).reject { |_k, v| v.nil? }
+
+    uri = /my-cases\?#{default_filters.to_param}/
+
+    stub_request(:get, uri)
+      .with(headers: { 'X-Api-Key' => ENV['INCOME_COLLECTION_API_KEY'] })
+      .to_return(status: 200, body: response_json)
+  end
+end
--- spec/support/mock_auth_helper.rb
+++ spec/support/mock_auth_helper.rb
@@ -11,22 +11,26 @@ module MockAuthHelper
   end
 
   def create_jwt_token(user_id: '100518888746922116647')
-    jwt_payload = {
-      'sub' => user_id,
-      'email' => 'hackney.user@test.hackney.gov.uk',
-      'iss' => 'Hackney',
-      'name' => 'Hackney User',
-      'groups' => ['group 1', 'group 2'],
-      'iat' => 1_570_462_732
-    }
-
-    jwt_token = JWT.encode(jwt_payload, ENV['HACKNEY_JWT_SECRET'], 'HS256')
+    jwt_token = build_jwt_token(user_id: user_id)
 
     cookie = "hackneyToken=#{jwt_token};"
 
     page.driver.browser.set_cookie(cookie)
 
     true
+  end
+
+  def build_jwt_token(user_id: nil, groups: nil)
+    jwt_payload = {
+      'sub' => user_id || Faker::Number.number(10),
+      'email' => 'hackney.user@test.hackney.gov.uk',
+      'iss' => 'Hackney',
+      'name' => 'Hackney User',
+      'groups' => groups || ['group 1', 'group 2'],
+      'iat' => 1_570_462_732
+    }
+
+    JWT.encode(jwt_payload, ENV['HACKNEY_JWT_SECRET'], 'HS256')
   end
 
   def given_i_am_logged_in
