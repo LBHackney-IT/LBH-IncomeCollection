e28977fdeca7d30575d9540cf5526ad1055b1bea
Richard Race <richard.race@madetech.com>
2019-11-06 16:56:15 +0000

    Add helper methods to the User Domain object for user type

--- lib/hackney/income/domain/user.rb
+++ lib/hackney/income/domain/user.rb
@@ -3,6 +3,14 @@ module Hackney
     module Domain
       class User
         attr_accessor :id, :name, :email, :groups
+
+        def leasehold_services?
+          groups.join(' ').include?('leasehold')
+        end
+
+        def income_collection?
+          groups.join(' ').include?('income')
+        end
       end
     end
   end
