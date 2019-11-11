28facf6c20ada498c0245b259c7dc42bf7dc09eb
Richard Race <richard.race@madetech.com>
2019-11-11 14:10:30 +0000

    Add helpful guide for running Feature tests in Chrome in dev

--- spec/support/chrome_test_helper.rb
+++ spec/support/chrome_test_helper.rb
@@ -1,8 +1,25 @@
 require 'selenium/webdriver'
 
+###################################
+# How to use:
+###################################
+#
+# You need to use RSpec in your own shell. You cannot use this inside Docker.
+#
+# `require 'support/chrome_test_helper'` at the top of your feature test.
+#
+# Use `js: true` to use Headless Chrome on a context/describe
+# Include `, driver: :selenium_chrome` to use Chrome in Head mode to help debug.
+#
+# E.g.: `scenario 'Testing something awesome', js: true, driver: :selenium_chrome do`
+#
+# Call `#pause` in your `scenario` to stop the test at a point so you can see
+# how everything looks/debug.
+#
+
 # Use `:selenium_chrome` for Header version
 Capybara.javascript_driver = :selenium_chrome_headless
 
-# Use `js: true` to use Headless Chrome on a context/describe
-# Include `, driver: :selenium_chrome` to use Chrome in Head mode to help
-# debug.
+def pause
+  STDIN.gets
+end
