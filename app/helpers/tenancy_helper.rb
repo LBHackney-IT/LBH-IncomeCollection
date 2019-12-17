5adc5d2b4b3ba535553eab87a1ca3c1fa366dfdb
Elena <elena@madetech.com>
2019-12-17 10:33:21 +0000

    introduce action diary entry partial instead of using helper

--- app/helpers/tenancy_helper.rb
+++ app/helpers/tenancy_helper.rb
@@ -20,35 +20,4 @@ module TenancyHelper
       'Other'              => 'GEN'
     }
   end
-
-  def action_diary_comment(comment)
-    if comment.length > 100
-      <<~HTML
-        #{comment.slice!(truncate_comment(comment))}...
-        <details class="govuk-details" data-module="govuk-details">
-          <summary class="govuk-details__summary">
-            <span class="govuk-details__summary-text">
-              Continue reading
-            </span>
-          </summary>
-          <div class="govuk-details__text">#{comment}</div>
-        </details>
-        HTML
-      .html_safe
-    else
-      comment.html_safe
-    end
-  end
-
-  private
-
-  def truncate_comment(comment)
-    truncate(
-      comment,
-      length: 100,
-      omission: '',
-      separator: ' ',
-      escape: false
-    )
-  end
 end
