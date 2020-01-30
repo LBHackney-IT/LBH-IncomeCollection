6e1956c4dc8b222fd7ec19673713d927c5626fa1
Richard Race <richard.race@madetech.com>
2020-01-30 15:35:20 +0000

    Add Page Titles

--- app/helpers/worktray_helper.rb
+++ app/helpers/worktray_helper.rb
@@ -4,4 +4,13 @@ module WorktrayHelper
 
     tabs_to_hide_filter.select { |filter| filter.in?(params.keys) }.empty?
   end
+
+  def worktray_title
+    return 'Paused Case List' if params[:paused]
+    return 'Full Patch List' if params[:full_patch]
+    return 'Upcoming Evictions' if params[:upcoming_evictions]
+    return 'Upcoming Court Dates' if params[:upcoming_court_dates]
+
+    'Case Worktray'
+  end
 end
