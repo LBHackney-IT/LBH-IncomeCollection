3bf6ff80447712ef83551eb201f4e67730a148d1
csabagyorfi <csaba@madetech.com>
2020-06-22 14:39:10 +0100

    Add create agreements gateway

---
+++ lib/hackney/income/create_agreement_gateway.rb
@@ -0,0 +1,33 @@
+require 'uri'
+require 'net/http'
+
+module Hackney
+  module Income
+    class CreateAgreementGateway
+      def initialize(api_host:, api_key:)
+        @api_host = api_host
+        @api_key = api_key
+      end
+
+      def create_agreement(tenancy_ref:, agreement_type:, frequency:, amount:, start_date:)
+        body_data = {
+          agreement_type: agreement_type,
+          frequency: frequency,
+          amount: amount,
+          start_date: start_date
+        }.to_json
+
+        uri = URI.parse("#{@api_host}/v1/agreement/#{ERB::Util.url_encode(tenancy_ref)}/")
+        req = Net::HTTP::Post.new(uri.path)
+        req['Content-Type'] = 'application/json'
+        req['X-Api-Key'] = @api_key
+
+        response = Net::HTTP.start(uri.host, uri.port, use_ssl: (uri.scheme == 'https')) { |http| http.request(req, body_data) }
+
+        raise Exceptions::IncomeApiError.new(response), "when trying to create a new agreement using '#{uri}'" unless response.is_a? Net::HTTPSuccess
+
+        response.body
+      end
+    end
+  end
+end
---
+++ spec/lib/hackney/income/create_agreement_gateway_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe Hackney::Income::CreateAgreementGateway do
+  subject { described_class.new(api_host: 'https://example.com/api', api_key: 'skeleton') }
+
+  let(:request_params) do
+    {
+      tenancy_ref: Faker::Lorem.characters(number: 6),
+      agreement_type: 'informal',
+      frequency: %w[weekly monthly].sample,
+      amount: Faker::Commerce.price(range: 10...100),
+      start_date: Faker::Date.between(from: 2.days.ago, to: Date.today)
+    }
+  end
+
+  let(:json_request_body) do
+    {
+      agreement_type: request_params.fetch(:agreement_type),
+      frequency: request_params.fetch(:frequency),
+      amount: request_params.fetch(:amount),
+      start_date: request_params.fetch(:start_date)
+    }.to_json
+  end
+
+  context 'when sending a successful request to the API' do
+    before do
+      stub_request(:post, "https://example.com/api/v1/agreement/#{ERB::Util.url_encode(request_params.fetch(:tenancy_ref))}/")
+        .to_return(
+          status: 200
+        )
+    end
+
+    it 'should send the required params' do
+      subject.create_agreement(**request_params)
+
+      assert_requested(
+        :post,
+        "https://example.com/api/v1/agreement/#{ERB::Util.url_encode(request_params.fetch(:tenancy_ref))}/",
+        headers: { 'Content-Type': 'application/json', 'X-Api-Key': 'skeleton' },
+        body: json_request_body,
+        times: 1
+      )
+    end
+  end
+
+  context 'when receiving a 500 error from the API' do
+    before do
+      stub_request(:post, "https://example.com/api/v1/agreement/#{ERB::Util.url_encode(request_params.fetch(:tenancy_ref))}/")
+        .to_return(
+          status: 500
+        )
+    end
+
+    it 'should raise and error' do
+      expect do
+        subject.create_agreement(**request_params)
+      end.to raise_error(
+        Exceptions::IncomeApiError,
+        "[Income API error: Received 500 response] when trying to create a new agreement using 'https://example.com/api/v1/agreement/#{ERB::Util.url_encode(request_params.fetch(:tenancy_ref))}/'"
+      )
+      assert_requested(
+        :post,
+        "https://example.com/api/v1/agreement/#{ERB::Util.url_encode(request_params.fetch(:tenancy_ref))}/",
+        headers: { 'Content-Type': 'application/json', 'X-Api-Key': 'skeleton' },
+        body: json_request_body,
+        times: 1
+      )
+    end
+  end
+end
