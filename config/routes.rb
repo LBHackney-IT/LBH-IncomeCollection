87554a5ca03aab62d8e6c6a8151726c7a1bfa493
csabagyorfi <csaba@madetech.com>
2020-07-02 18:12:08 +0100

    Allow case workers to cancel an agreement

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,8 @@ Rails.application.routes.draw do
   get '/tenancies/:tenancy_ref/agreement/new', to: 'agreements#new', as: :new_agreement
   post '/tenancies/:tenancy_ref/agreement/create', to: 'agreements#create', as: :create_agreement
   get '/tenancies/:tenancy_ref/agreement/:id/show', to: 'agreements#show', as: :show_agreement
+  get '/tenancies/:tenancy_ref/agreement/:id/cancel', to: 'agreements#confirm_cancellation', as: :confirm_agreement_cancellation
+  post '/tenancies/:tenancy_ref/agreement/:id/cancel', to: 'agreements#cancel', as: :cancel_agreement
 
   get '/login', to: 'hackney_auth_session#new'
   get '/logout', to: 'hackney_auth_session#destroy'
