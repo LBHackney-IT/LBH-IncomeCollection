73436063776620bc77a411d4c3d02871c740bcc9
csabagyorfi <csaba@madetech.com>
2020-07-10 10:11:43 +0100

    Resolve assets references

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,11 @@
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+import "core-js/stable"
+import "regenerator-runtime/runtime"
+import "../stylesheets/application.scss";
 
 console.log('Hello World from Webpacker')
