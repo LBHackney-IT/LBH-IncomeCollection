e555be91b99b3bc8400074e5b9d7cecd400ac17d
csabagyorfi <csaba@madetech.com>
2020-07-13 18:52:27 +0100

    Add end date calculator
    We want to be able to generate an end date when creating new agreement,
       and this date should be dinamicaly updated based on the agreement conditions.

--- app/assets/javascripts/income_collection/EndDateCalculator.js
+++ app/assets/javascripts/income_collection/EndDateCalculator.js
@@ -1,3 +1,20 @@
-window.EndDateCalculator = function EndDateCalculator () {
-  return 'FOOO';
+try { var moment = require('moment') } catch(err){}
+if (!moment) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js';
+  document.body.appendChild(script);
+}
+
+window.EndDateCalculator = function EndDateCalculator (totalArrears, startDate, frequency, amount) {
+  if (!totalArrears || !startDate || !frequency || !amount) return '';
+  if (new Date(startDate) == 'Invalid Date') return '';
+
+  var numberOfInstalments = Math.ceil(parseFloat(totalArrears) / parseFloat(amount)) - 1;
+  const frequencyOfPayment = (frequency == 'Monthly') ? 'months' : 'weeks'
+
+  if (frequency == 'Fortnightly') numberOfInstalments = numberOfInstalments * 2;
+  if (frequency == '4 weekly') numberOfInstalments = numberOfInstalments * 4;
+
+  return moment(startDate).add(numberOfInstalments, frequencyOfPayment).format('D MMMM YYYY');
 };
--- spec/javascript/EndDateCalculator.spec.js
+++ spec/javascript/EndDateCalculator.spec.js
@@ -1,5 +1,94 @@
 import EndDateCalculator from '../../app/assets/javascripts/income_collection/EndDateCalculator.js'
 
-test('returns an empty string', () => {
-  expect(window.EndDateCalculator()).toEqual('FOOO')
-})
+describe('EndDateCalculator', function() {
+  describe('when the params are invalid', function() {    
+    it('returns empty string if any of the params are missing', function() {
+      var totalArrears = '1000';
+      var startDate = '2020-12-1';
+      var frequency = '';
+      var amount = '50';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual('')
+    });
+
+    it('returns empty string when the start date is invalid', function() {
+      var totalArrears = '1000';
+      var startDate = 'foo';
+      var frequency = "Weekly";
+      var amount = '50';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual('')
+    });
+  });
+
+  describe('Weekly payments', function() {
+    it('calcules end date when arrears paid in a single instalment', function() {
+      var frequency = 'Weekly';
+      var totalArrears = '20';
+      var amount = '20';
+      var startDate = '2020-12-01';
+      var expectedEndDate = '1 December 2020';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual(expectedEndDate)
+    });
+
+    it('calculates end date when its exactly 2 weeks to complete', function() {
+      var frequency = 'Weekly';
+      var totalArrears = '100';
+      var amount = '50';
+      var startDate = '2020-12-01';
+      var expectedEndDate = '8 December 2020';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual(expectedEndDate)
+    });
+
+    it('calculates end date when its exactly 3 weeks to complete', function() {
+      var frequency = 'Weekly';
+      var totalArrears = '150';
+      var amount = '50';
+      var startDate = '2020-12-01';
+      var expectedEndDate = '15 December 2020';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual(expectedEndDate)
+    });
+
+    it('calculates end date when the last payment is less than the agreed amount', function() {
+      var frequency = 'Weekly';
+      var totalArrears = '120';
+      var amount = '50';
+      var startDate = '2020-12-01';
+      var expectedEndDate = '15 December 2020';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual(expectedEndDate)
+    });
+  });
+
+  describe('Monthly payments', function() {
+    it('calcules end date when arrears paid in 2 instalment', function() {
+      var frequency = 'Monthly';
+      var totalArrears = '40';
+      var amount = '20';
+      var startDate = '2020-12-01';
+      var expectedEndDate = '1 January 2021';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual(expectedEndDate)
+    });
+  }); 
+
+  describe('Fortnightly payments', function() {
+    it('calcules end date when arrears paid in 2 instalment', function() {
+      var frequency = 'Fortnightly';
+      var totalArrears = '40';
+      var amount = '20';
+      var startDate = '2020-12-01';
+      var expectedEndDate = '15 December 2020';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual(expectedEndDate)
+    });
+  }); 
+
+  describe('4 weekly payments', function() {
+    it('calcules end date when arrears paid in 3 instalment', function() {
+      var frequency = '4 weekly';
+      var totalArrears = '50';
+      var amount = '20';
+      var startDate = '2020-12-01';
+      var expectedEndDate = '26 January 2021';
+      expect(window.EndDateCalculator(totalArrears, startDate, frequency, amount)).toEqual(expectedEndDate)
+    });
+  }); 
+});
+
+
