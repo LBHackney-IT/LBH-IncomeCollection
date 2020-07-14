e17b4e1ce91427dd331d683ebb304dce313b62b7
csabagyorfi <csaba@madetech.com>
2020-07-14 17:41:20 +0100

    Add end date method to agreement helper
    So we don't need to call the JS method on pages where the agreement
    details are not editable

--- app/helpers/agreement_helper.rb
+++ app/helpers/agreement_helper.rb
@@ -7,4 +7,22 @@ module AgreementHelper
       'Calendar monthly' => 'Monthly'
     }
   end
+
+  def show_end_date(total_arrears:, start_date:, frequency:, amount:)
+    return nil if total_arrears.nil? || start_date.nil? || frequency.nil? || amount.nil?
+
+    start_date = Date.parse(start_date)
+    number_of_instalments = total_arrears.to_f.fdiv(amount.to_f).ceil - 1
+    end_date = if frequency == 'weekly'
+                 start_date + number_of_instalments.weeks
+               elsif frequency == 'fortnightly'
+                 start_date + (number_of_instalments * 2).weeks
+               elsif frequency == '4 weekly'
+                 start_date + (number_of_instalments * 4).weeks
+               else
+                 start_date + number_of_instalments.months
+               end
+
+    format_date(end_date.to_s)
+  end
 end
