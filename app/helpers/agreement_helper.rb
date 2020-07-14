2ede59a9d5e5df98a5bed5d7dfcb2bd6ccd7e85b
csabagyorfi <csaba@madetech.com>
2020-07-14 09:30:27 +0100

    Add new payment frequencies to agreement helper

--- app/helpers/agreement_helper.rb
+++ app/helpers/agreement_helper.rb
@@ -2,7 +2,9 @@ module AgreementHelper
   def frequency_of_payments
     {
       'Weekly' => 'Weekly',
-      'Monthly' => 'Monthly'
+      'Fortnightly' => 'Fortnightly',
+      '4 weekly' => '4 weekly',
+      'Calendar monthly' => 'Monthly'
     }
   end
 end
