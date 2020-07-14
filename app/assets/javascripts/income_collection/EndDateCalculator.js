8e1ad1507f16ff534a16a9d46f6744868c1d5381
csabagyorfi <csaba@madetech.com>
2020-07-14 10:55:35 +0100

    Call end date calculator on show agreement page

--- app/assets/javascripts/income_collection/EndDateCalculator.js
+++ app/assets/javascripts/income_collection/EndDateCalculator.js
@@ -1,10 +1,4 @@
 try { var moment = require('moment') } catch(err){}
-if (!moment) {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js';
-  document.body.appendChild(script);
-}
 
 window.EndDateCalculator = function EndDateCalculator (totalArrears, startDate, frequency, amount) {
   if (!totalArrears || !startDate || !frequency || !amount) return '';
