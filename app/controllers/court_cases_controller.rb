eba881f177cf91e7810746ed76a926e4f6644ede
Ninamma <ninamma.rai@outlook.com>
2020-07-27 11:53:14 +0100

    Add court cases controller and create court case use case

---
+++ app/controllers/court_cases_controller.rb
@@ -0,0 +1,42 @@
+class CourtCasesController < ApplicationController
+  protect_from_forgery
+  before_action { redirect_to worktray_path unless FeatureFlag.active?('create_informal_agreements') }
+
+  def new
+    @tenancy = use_cases.view_tenancy.execute(tenancy_ref: tenancy_ref)
+    @court_decision_date = params['court_decision_date']
+    @court_outcome = params['court_outcome']
+    @balance_at_outcome_date = params['balance_at_outcome_date']
+  end
+
+  def create
+    use_cases.create_court_case.execute(
+      tenancy_ref: tenancy_ref,
+      created_by: @current_user.name,
+      **court_case_params
+    )
+    redirect_to show_success_court_case_path
+  rescue Exceptions::IncomeApiError => e
+    flash[:notice] = "An error occurred: #{e.message}"
+    redirect_to new_court_case_path(tenancy_ref: tenancy_ref, **court_case_params)
+  end
+
+  def show_success
+    flash[:notice] = 'Successfully created a new court case'
+    redirect_to tenancy_path(id: tenancy_ref)
+  end
+  
+  private
+
+  def tenancy_ref
+    @tenancy_ref ||= params.fetch(:tenancy_ref)
+  end
+
+  def court_case_params
+    {
+      court_decision_date: params.fetch(:court_decision_date),
+      court_outcome: params.fetch(:court_outcome),
+      balance_at_outcome_date: params.fetch(:balance_at_outcome_date)
+    }
+  end
+end
