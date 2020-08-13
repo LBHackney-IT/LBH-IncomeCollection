7810a5481d8c7d7d4b6788ab61e7be777d8749e9
Ninamma <ninamma.rai@outlook.com>
2020-08-13 14:29:35 +0100

    Add update-court-case method in the court case gateway

--- spec/lib/hackney/income/court_cases_gateway_spec.rb
+++ spec/lib/hackney/income/court_cases_gateway_spec.rb
@@ -130,4 +130,78 @@ describe Hackney::Income::CourtCasesGateway do
       end
     end
   end
+
+  describe '#update_court_case' do
+    let(:id) { Faker::Number.number(digits: 3) }
+    let(:court_date) { Faker::Date.between(from: 2.days.ago, to: Date.today) }
+    
+    let(:request_params) do
+      {
+        id: id,
+        court_date: court_date,
+        court_outcome: nil,
+        balance_on_court_outcome_date: nil,
+        strike_out_date: nil,
+        terms: nil,
+        disrepair_counter_claim: nil
+      }
+    end
+
+    let(:json_request_body) do
+      {
+        court_date: request_params.fetch(:court_date),
+        court_outcome: request_params.fetch(:court_outcome),
+        balance_on_court_outcome_date: request_params.fetch(:balance_on_court_outcome_date),
+        strike_out_date: request_params.fetch(:strike_out_date),
+        terms: request_params.fetch(:terms),
+        disrepair_counter_claim: request_params.fetch(:disrepair_counter_claim)
+      }.to_json
+    end
+
+    context 'when sending a successful request to the API' do
+      before do
+        stub_request(:patch, "https://example.com/api/v1/court_case/#{ERB::Util.url_encode(id)}/update")
+          .to_return(
+            status: 200
+          )
+      end
+
+      it 'should send the required params' do
+        subject.update_court_case(court_case_params: request_params)
+
+        assert_requested(
+          :patch,
+          "https://example.com/api/v1/court_case/#{ERB::Util.url_encode(id)}/update",
+          headers: { 'Content-Type': 'application/json', 'X-Api-Key': 'skeleton' },
+          body: json_request_body,
+          times: 1
+        )
+      end
+    end
+
+    context 'when receiving a 500 error from the API' do
+      before do
+        stub_request(:patch, "https://example.com/api/v1/court_case/#{ERB::Util.url_encode(id)}/update")
+          .to_return(
+            status: 500
+          )
+      end
+
+      it 'should raise and error' do
+        expect do
+          subject.update_court_case(court_case_params: request_params)
+        end.to raise_error(
+          Exceptions::IncomeApiError,
+          "[Income API error: Received 500 response] when trying to update the court case using 'https://example.com/api/v1/court_case/#{ERB::Util.url_encode(id)}/update'"
+        )
+        assert_requested(
+          :patch,
+          "https://example.com/api/v1/court_case/#{ERB::Util.url_encode(id)}/update",
+          headers: { 'Content-Type': 'application/json', 'X-Api-Key': 'skeleton' },
+          body: json_request_body,
+          times: 1
+        )
+      end
+    end
+  end
 end
