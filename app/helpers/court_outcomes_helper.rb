95d13b3ef213408249c254e8c2348d07938c93e1
csabagyorfi <csaba@madetech.com>
2020-08-24 16:52:08 +0100

    Add missing court outcomes
    We are syncinc these so we need to include them in the helper otherwise
    we cannot display these correctly

--- app/helpers/court_outcomes_helper.rb
+++ app/helpers/court_outcomes_helper.rb
@@ -15,14 +15,17 @@ module CourtOutcomesHelper
 
   def court_outcomes
     {
+      'ADT' => 'Adjourned on Terms',
       'AGP' => 'Adjourned generally with permission to restore',
       'AND' => 'Adjourned to the next open date',
       'AAH' => 'Adjourned to another hearing date',
       'ADH' => 'Adjourned for directions hearing',
+      'OPD' => 'Outright Possession (with Date)',
+      'OPF' => 'Outright Possession (Forthwith)',
       'SOT' => 'Suspension on terms',
       'SOE' => 'Stay of Execution',
-      'WIT' => 'Withdrawn on the day',
-      'STO' => 'Struck out'
+      'STO' => 'Struck out',
+      'WIT' => 'Withdrawn on the day'
     }
   end
 
