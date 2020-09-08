14ca4152fe2d68d03fd12d9c2fbe04a139f75e1c
csabagyorfi <csaba@madetech.com>
2020-09-08 13:47:26 +0100

    Update JS EndDateCalculator for vairable payments

--- app/assets/javascripts/income_collection/EndDateCalculator.js
+++ app/assets/javascripts/income_collection/EndDateCalculator.js
@@ -1,8 +1,9 @@
 try { var moment = require('moment') } catch(err){}
 
-window.EndDateCalculator = function EndDateCalculator (totalArrears, startDate, frequency, amount) {
+window.EndDateCalculator = function EndDateCalculator (totalArrears, startDate, frequency, amount, initialPaymentAmount) {
   if (!totalArrears || !startDate || !frequency || !amount) return '';
   if (new Date(startDate) == 'Invalid Date') return '';
+  if (initialPaymentAmount) totalArrears = parseFloat(totalArrears) - parseFloat(initialPaymentAmount);
 
   var numberOfInstalments = Math.ceil(parseFloat(totalArrears) / parseFloat(amount)) - 1;
   const frequencyOfPayment = (frequency.toLowerCase() == 'monthly') ? 'months' : 'weeks'
