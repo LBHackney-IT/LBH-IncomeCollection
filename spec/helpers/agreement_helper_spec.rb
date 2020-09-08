d5fccde4acccfc3ae13105ef5a5d98f4919810dd
csabagyorfi <csaba@madetech.com>
2020-09-08 13:27:29 +0100

    Update agreement helper to show end date when its variable payment

--- spec/helpers/agreement_helper_spec.rb
+++ spec/helpers/agreement_helper_spec.rb
@@ -85,5 +85,33 @@ describe AgreementHelper do
         expect(helper.show_end_date(params)).to eq(expected_end_date)
       end
     end
+
+    context 'when there is an initial payment amount before or on the date of first instalment' do
+      let(:params) do
+        {
+          total_arrears: '50',
+          start_date: '2020-12-01',
+          frequency: 'weekly',
+          amount: '20',
+          initial_payment_amount: '30'
+        }
+      end
+      it 'calculates the end date for a single instalment' do
+        expected_end_date = 'December 1st, 2020'
+        expect(helper.show_end_date(params)).to eq(expected_end_date)
+      end
+
+      it 'calculates end date when its exactly 2 instalments to complete' do
+        params[:total_arrears] = 70
+        expected_end_date = 'December 8th, 2020'
+        expect(helper.show_end_date(params)).to eq(expected_end_date)
+      end
+
+      it 'calculates end date when the last payment is less than the agreed amount' do
+        params[:total_arrears] = 80
+        expected_end_date = 'December 15th, 2020'
+        expect(helper.show_end_date(params)).to eq(expected_end_date)
+      end
+    end
   end
 end
