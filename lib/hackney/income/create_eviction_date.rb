44bc4ccde63c5f2f8ceee02e1a708a859466a60b
Elena <elena@madetech.com>
2020-09-23 12:46:29 +0100

    create eviction date use case

---
+++ lib/hackney/income/create_eviction_date.rb
@@ -0,0 +1,18 @@
+module Hackney
+  module Income
+    class CreateEvictionDate
+      def initialize(eviction_gateway:)
+        @eviction_gateway = eviction_gateway
+      end
+
+      def execute(eviction_params:)
+        eviction_params = {
+          tenancy_ref: eviction_params[:tenancy_ref],
+          date: eviction_params[:eviction_date]
+        }
+
+        @eviction_gateway.create_eviction(params: eviction_params)
+      end
+    end
+  end
+end
